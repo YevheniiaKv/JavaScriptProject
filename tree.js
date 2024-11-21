fbaebfeb479b482092b6b81ae048f18e750c0d0d
Yevheniia Kviatkovska <yevkviat@gmail.com>
2024-11-21 22:31:02 +0200

    Add file tree.js

---
+++ tree.js
@@ -0,0 +1,9 @@
+console.log("Hi, how are you?");
+console.log("Hi,\nVera!");
+console.log("I'm fine. Merry Christmas\nto you!");
+console.log("   /\\   ");
+console.log("  /  \\");
+console.log(" /    \\");
+console.log(" ------");
+console.log("   ||   ");
+document.write("Hello! &nbsp;life!");
