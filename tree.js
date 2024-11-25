b53cf670bff557b5e7fa8ed87ea21ea93e56e303
Yevheniia Kviatkovska <111862532+YevheniiaKv@users.noreply.github.com>
2024-11-25 22:57:41 +0200

    Delete tree.js

--- tree.js
+++
@@ -1,9 +0,0 @@
-console.log("Hi, how are you?");
-console.log("Hi,\nVera!");
-console.log("I'm fine. Merry Christmas\nto you!");
-console.log("   /\\   ");
-console.log("  /  \\");
-console.log(" /    \\");
-console.log(" ------");
-console.log("   ||   ");
-document.write("Hello! &nbsp;life!");
